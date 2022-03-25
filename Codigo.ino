0778a7e4eb17e91c58a89a997c5d0c7e4c1c8d08
jelambrar96 <jelambrar@gmail.com>
2022-03-25 12:02:36 -0500

    replace patterns

--- Codigo.ino
+++ Codigo.ino
@@ -1,252 +1,110 @@
+// ----------------------------------------------------------------------------
+// INIT ALL LED PINS AS OUTPUTS
+// ----------------------------------------------------------------------------
 void setup() {
-pinMode(0, OUTPUT);
-pinMode(1, OUTPUT);
-pinMode(2, OUTPUT);
-pinMode(3, OUTPUT);
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-pinMode(6, OUTPUT);
-pinMode(7, OUTPUT);
+    pinMode(0, OUTPUT);
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
 }
 
+// ----------------------------------------------------------------------------
+// MAIN LOOP
+// ----------------------------------------------------------------------------
 void loop() {
 
-//Patron 01
-digitalWrite(0, LOW); 
-delay(300);
-digitalWrite(1, LOW); 
-delay(300);
-digitalWrite(2, LOW); 
-delay(300);
-digitalWrite(3, LOW); 
-delay(300);
-digitalWrite(4, LOW); 
-delay(300);
-digitalWrite(5, LOW); 
-delay(300);
-digitalWrite(6, LOW); 
-delay(300);
-digitalWrite(7, LOW);
-delay(300);
-  
-digitalWrite(0, HIGH); 
-delay(200);
-digitalWrite(1, HIGH); 
-delay(200);
-digitalWrite(2, HIGH); 
-delay(200);
-digitalWrite(3, HIGH); 
-delay(200);
-digitalWrite(4, HIGH); 
-delay(200);
-digitalWrite(5, HIGH); 
-delay(200);
-digitalWrite(6, HIGH); 
-delay(200);
-digitalWrite(7, HIGH);
-delay(200);
+    pattern1(3);
+    pattern2(3);
+    pattern3(8);
 
-//Patron 02
-digitalWrite(7, LOW);
-delay(300);
-digitalWrite(6, LOW); 
-delay(300);
-digitalWrite(5, LOW); 
-delay(300);
-digitalWrite(4, LOW); 
-delay(300);
-digitalWrite(3, LOW); 
-delay(300);
-digitalWrite(2, LOW); 
-delay(300);
-digitalWrite(1, LOW); 
-delay(300);
-digitalWrite(0, LOW); 
-delay(300);
+}
 
-digitalWrite(7, HIGH);
-delay(200);
-digitalWrite(6, HIGH); 
-delay(200);
-digitalWrite(5, HIGH); 
-delay(200);
-digitalWrite(4, HIGH); 
-delay(200);
-digitalWrite(3, HIGH); 
-delay(200);
-digitalWrite(2, HIGH); 
-delay(200);
-digitalWrite(1, HIGH); 
-delay(200);
-digitalWrite(0, HIGH); 
-delay(200);
 
-//Patron 03
-digitalWrite(7, LOW);
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
-digitalWrite(4, HIGH); 
-digitalWrite(3, LOW); 
-digitalWrite(2, HIGH); 
-digitalWrite(1, LOW); 
-digitalWrite(0, HIGH); 
-delay(300);
+// ----------------------------------------------------------------------------
+// PATTERN 1
+// ----------------------------------------------------------------------------
+void pattern1(int times) {
 
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
-digitalWrite(4, LOW); 
-digitalWrite(3, HIGH); 
-digitalWrite(2, LOW); 
-digitalWrite(1, HIGH); 
-digitalWrite(0, LOW); 
-delay(300); //Repite 1
+    // TURN ON ALL LEDS
+    for (int i = 0; i < 8; ++i) {
+        digitalWrite(i, HIGH);
+    }
 
-digitalWrite(7, LOW);
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
-digitalWrite(4, HIGH); 
-digitalWrite(3, LOW); 
-digitalWrite(2, HIGH); 
-digitalWrite(1, LOW); 
-digitalWrite(0, HIGH); 
-delay(300);
+    // REPEAT "TIMES" TIMES
+    for (int i = 0; i < times; ++i) {
 
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
-digitalWrite(4, LOW); 
-digitalWrite(3, HIGH); 
-digitalWrite(2, LOW); 
-digitalWrite(1, HIGH); 
-digitalWrite(0, LOW); 
-delay(300); //Repite 2
+        // TURN OFF LED
+        for (int i = 0; i < 8; ++i) {
+            digitalWrite(i, LOW);
+            delay(300);
+        }
 
-digitalWrite(7, LOW);
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
-digitalWrite(4, HIGH); 
-digitalWrite(3, LOW); 
-digitalWrite(2, HIGH); 
-digitalWrite(1, LOW); 
-digitalWrite(0, HIGH); 
-delay(300);
+        // TURN ON ALL LEDS
+        for (int i = 0; i < 8; ++i) {
+            digitalWrite(i, HIGH);
+            delay(300);
+        }
+    }
 
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
-digitalWrite(4, LOW); 
-digitalWrite(3, HIGH); 
-digitalWrite(2, LOW); 
-digitalWrite(1, HIGH); 
-digitalWrite(0, LOW); 
-delay(300); //Repite 3
+}
 
-digitalWrite(7, LOW);
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
-digitalWrite(4, HIGH); 
-digitalWrite(3, LOW); 
-digitalWrite(2, HIGH); 
-digitalWrite(1, LOW); 
-digitalWrite(0, HIGH); 
-delay(300);
 
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
-digitalWrite(4, LOW); 
-digitalWrite(3, HIGH); 
-digitalWrite(2, LOW); 
-digitalWrite(1, HIGH); 
-digitalWrite(0, LOW); 
-delay(300); //Repite 4
+// ----------------------------------------------------------------------------
+// PATTERN 2
+// ----------------------------------------------------------------------------
+void pattern2(int times) {
+    
+    // TURN ON ALL LEDS
+    for (int i = 0; i < 8; ++i) {
+        digitalWrite(i, HIGH);
+    }
 
-digitalWrite(7,LOW);
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
-digitalWrite(4, HIGH); 
-digitalWrite(3, LOW); 
-digitalWrite(2, HIGH); 
-digitalWrite(1, LOW); 
-digitalWrite(0, HIGH); 
-delay(300);
+    // REPEAT "TIMES" TIMES
+    for (int i = 0; i < times; ++i) {
 
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
-digitalWrite(4, LOW); 
-digitalWrite(3, HIGH); 
-digitalWrite(2, LOW); 
-digitalWrite(1, HIGH); 
-digitalWrite(0, LOW); 
-delay(300); //Repite 5
+        // TURN OFF LED
+        for (int i = 7; i < 0; --i) {
+            digitalWrite(i, LOW);
+            delay(300);
+        }
 
-digitalWrite(7, LOW);
-digitalWrite(6, HIGH);
-digitalWrite(5, LOW); 
-digitalWrite(4, HIGH); 
-digitalWrite(3, LOW); 
-digitalWrite(2, HIGH); 
-digitalWrite(1, LOW); 
-digitalWrite(0, HIGH); 
-delay(300);
+        // TURN ON ALL LEDS
+        for (int i = 7; i < 0; --i) {
+            digitalWrite(i, HIGH);
+            delay(300);
+        }
+    }
+}
 
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
-digitalWrite(4, LOW); 
-digitalWrite(3, HIGH); 
-digitalWrite(2, LOW); 
-digitalWrite(1, HIGH); 
-digitalWrite(0, LOW); 
-delay(300); //Repite 6
 
-digitalWrite(7, LOW);
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
-digitalWrite(4, HIGH); 
-digitalWrite(3, LOW); 
-digitalWrite(2, HIGH); 
-digitalWrite(1, LOW); 
-digitalWrite(0, HIGH); 
-delay(300);
+// ----------------------------------------------------------------------------
+// PATTERN 3
+// ----------------------------------------------------------------------------
+void pattern3(int times) {
 
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
-digitalWrite(4, LOW); 
-digitalWrite(3, HIGH); 
-digitalWrite(2, LOW); 
-digitalWrite(1, HIGH); 
-digitalWrite(0, LOW); 
-delay(300); //Repite 7
+    // TURN ON ALL LEDS
+    for (int i = 0; i < 8; ++i) {
+        digitalWrite(i, HIGH);
+    }
 
-digitalWrite(7, LOW);
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
-digitalWrite(4, HIGH); 
-digitalWrite(3, LOW); 
-digitalWrite(2, HIGH); 
-digitalWrite(1, LOW); 
-digitalWrite(0, HIGH); 
-delay(300);
+    // REPEAT "TIMES" TIMES
+    for (int i = 0; i < times; ++i) {
 
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
-digitalWrite(4, LOW); 
-digitalWrite(3, HIGH); 
-digitalWrite(2, LOW); 
-digitalWrite(1, HIGH); 
-digitalWrite(0, LOW); 
-delay(300); //Repite 8
+        // ON 
+        for (int i = 0; i < 8; ++i) {
+            digitalWrite(i, i % 2 == 0 ? LOW : HIGH);
+        }
+        delay(300);
 
-digitalWrite(7, HIGH);
-digitalWrite(5, HIGH); 
-digitalWrite(3, HIGH); 
-digitalWrite(1, HIGH); 
-delay(200);
+        for (int i = 0; i < 8; ++i) {
+            digitalWrite(i, i % 2 == 0 ? HIGH : LOW);
+        }
+        delay(300);
 
+    }    
 
 }

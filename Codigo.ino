cb5c527391aaf4ec8227a86094ebb6405db8c6b0
Glohallo <gloriacarrascal53@gmail.com>
2022-03-24 23:58:44 -0500

    Update Codigo.ino

--- Codigo.ino
+++ Codigo.ino
@@ -3,10 +3,10 @@ pinMode(0, OUTPUT);
 pinMode(1, OUTPUT);
 pinMode(2, OUTPUT);
 pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
 pinMode(5, OUTPUT);
 pinMode(6, OUTPUT);
 pinMode(7, OUTPUT);
-pinMode(8, OUTPUT);
 }
 
 void loop() {
@@ -20,13 +20,13 @@ digitalWrite(2, LOW);
 delay(300);
 digitalWrite(3, LOW); 
 delay(300);
+digitalWrite(4, LOW); 
+delay(300);
 digitalWrite(5, LOW); 
 delay(300);
 digitalWrite(6, LOW); 
 delay(300);
-digitalWrite(7, LOW); 
-delay(300);
-digitalWrite(8, LOW);
+digitalWrite(7, LOW);
 delay(300);
   
 digitalWrite(0, HIGH); 
@@ -37,23 +37,23 @@ digitalWrite(2, HIGH);
 delay(200);
 digitalWrite(3, HIGH); 
 delay(200);
+digitalWrite(4, HIGH); 
+delay(200);
 digitalWrite(5, HIGH); 
 delay(200);
 digitalWrite(6, HIGH); 
 delay(200);
-digitalWrite(7, HIGH); 
-delay(200);
-digitalWrite(8, HIGH);
+digitalWrite(7, HIGH);
 delay(200);
 
 //Patron 02
-digitalWrite(8, LOW);
-delay(300);
-digitalWrite(7, LOW); 
+digitalWrite(7, LOW);
 delay(300);
 digitalWrite(6, LOW); 
 delay(300);
 digitalWrite(5, LOW); 
+delay(300);
+digitalWrite(4, LOW); 
 delay(300);
 digitalWrite(3, LOW); 
 delay(300);
@@ -64,13 +64,13 @@ delay(300);
 digitalWrite(0, LOW); 
 delay(300);
 
-digitalWrite(8, HIGH);
-delay(200);
-digitalWrite(7, HIGH); 
+digitalWrite(7, HIGH);
 delay(200);
 digitalWrite(6, HIGH); 
 delay(200);
 digitalWrite(5, HIGH); 
+delay(200);
+digitalWrite(4, HIGH); 
 delay(200);
 digitalWrite(3, HIGH); 
 delay(200);
@@ -82,170 +82,171 @@ digitalWrite(0, HIGH);
 delay(200);
 
 //Patron 03
-digitalWrite(8, LOW);
-digitalWrite(7, HIGH); 
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH); 
+digitalWrite(5, LOW); 
+digitalWrite(4, HIGH); 
 digitalWrite(3, LOW); 
 digitalWrite(2, HIGH); 
 digitalWrite(1, LOW); 
 digitalWrite(0, HIGH); 
 delay(300);
 
-digitalWrite(8, HIGH);
-digitalWrite(7, LOW); 
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW); 
+digitalWrite(5, HIGH); 
+digitalWrite(4, LOW); 
 digitalWrite(3, HIGH); 
 digitalWrite(2, LOW); 
 digitalWrite(1, HIGH); 
 digitalWrite(0, LOW); 
 delay(300); //Repite 1
 
-digitalWrite(8, LOW);
-digitalWrite(7, HIGH); 
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH); 
+digitalWrite(5, LOW); 
+digitalWrite(4, HIGH); 
 digitalWrite(3, LOW); 
 digitalWrite(2, HIGH); 
 digitalWrite(1, LOW); 
 digitalWrite(0, HIGH); 
 delay(300);
 
-digitalWrite(8, HIGH);
-digitalWrite(7, LOW); 
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW); 
+digitalWrite(5, HIGH); 
+digitalWrite(4, LOW); 
 digitalWrite(3, HIGH); 
 digitalWrite(2, LOW); 
 digitalWrite(1, HIGH); 
 digitalWrite(0, LOW); 
 delay(300); //Repite 2
 
-digitalWrite(8, LOW);
-digitalWrite(7, HIGH); 
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH); 
+digitalWrite(5, LOW); 
+digitalWrite(4, HIGH); 
 digitalWrite(3, LOW); 
 digitalWrite(2, HIGH); 
 digitalWrite(1, LOW); 
 digitalWrite(0, HIGH); 
 delay(300);
 
-digitalWrite(8, HIGH);
-digitalWrite(7, LOW); 
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW); 
+digitalWrite(5, HIGH); 
+digitalWrite(4, LOW); 
 digitalWrite(3, HIGH); 
 digitalWrite(2, LOW); 
 digitalWrite(1, HIGH); 
 digitalWrite(0, LOW); 
 delay(300); //Repite 3
 
-digitalWrite(8, LOW);
-digitalWrite(7, HIGH); 
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH); 
+digitalWrite(5, LOW); 
+digitalWrite(4, HIGH); 
 digitalWrite(3, LOW); 
 digitalWrite(2, HIGH); 
 digitalWrite(1, LOW); 
 digitalWrite(0, HIGH); 
 delay(300);
 
-digitalWrite(8, HIGH);
-digitalWrite(7, LOW); 
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW); 
+digitalWrite(5, HIGH); 
+digitalWrite(4, LOW); 
 digitalWrite(3, HIGH); 
 digitalWrite(2, LOW); 
 digitalWrite(1, HIGH); 
 digitalWrite(0, LOW); 
 delay(300); //Repite 4
 
-digitalWrite(8,LOW);
-digitalWrite(7, HIGH); 
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
+digitalWrite(7,LOW);
+digitalWrite(6, HIGH); 
+digitalWrite(5, LOW); 
+digitalWrite(4, HIGH); 
 digitalWrite(3, LOW); 
 digitalWrite(2, HIGH); 
 digitalWrite(1, LOW); 
 digitalWrite(0, HIGH); 
 delay(300);
 
-digitalWrite(8, HIGH);
-digitalWrite(7, LOW); 
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW); 
+digitalWrite(5, HIGH); 
+digitalWrite(4, LOW); 
 digitalWrite(3, HIGH); 
 digitalWrite(2, LOW); 
 digitalWrite(1, HIGH); 
 digitalWrite(0, LOW); 
 delay(300); //Repite 5
 
-digitalWrite(8, LOW);
-digitalWrite(7, HIGH);
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW); 
+digitalWrite(4, HIGH); 
 digitalWrite(3, LOW); 
 digitalWrite(2, HIGH); 
 digitalWrite(1, LOW); 
 digitalWrite(0, HIGH); 
 delay(300);
 
-digitalWrite(8, HIGH);
-digitalWrite(7, LOW); 
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW); 
+digitalWrite(5, HIGH); 
+digitalWrite(4, LOW); 
 digitalWrite(3, HIGH); 
 digitalWrite(2, LOW); 
 digitalWrite(1, HIGH); 
 digitalWrite(0, LOW); 
 delay(300); //Repite 6
 
-digitalWrite(8, LOW);
-digitalWrite(7, HIGH); 
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH); 
+digitalWrite(5, LOW); 
+digitalWrite(4, HIGH); 
 digitalWrite(3, LOW); 
 digitalWrite(2, HIGH); 
 digitalWrite(1, LOW); 
 digitalWrite(0, HIGH); 
 delay(300);
 
-digitalWrite(8, HIGH);
-digitalWrite(7, LOW); 
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW); 
+digitalWrite(5, HIGH); 
+digitalWrite(4, LOW); 
 digitalWrite(3, HIGH); 
 digitalWrite(2, LOW); 
 digitalWrite(1, HIGH); 
 digitalWrite(0, LOW); 
 delay(300); //Repite 7
 
-digitalWrite(8, LOW);
-digitalWrite(7, HIGH); 
-digitalWrite(6, LOW); 
-digitalWrite(5, HIGH); 
+digitalWrite(7, LOW);
+digitalWrite(6, HIGH); 
+digitalWrite(5, LOW); 
+digitalWrite(4, HIGH); 
 digitalWrite(3, LOW); 
 digitalWrite(2, HIGH); 
 digitalWrite(1, LOW); 
 digitalWrite(0, HIGH); 
 delay(300);
 
-digitalWrite(8, HIGH);
-digitalWrite(7, LOW); 
-digitalWrite(6, HIGH); 
-digitalWrite(5, LOW); 
+digitalWrite(7, HIGH);
+digitalWrite(6, LOW); 
+digitalWrite(5, HIGH); 
+digitalWrite(4, LOW); 
 digitalWrite(3, HIGH); 
 digitalWrite(2, LOW); 
 digitalWrite(1, HIGH); 
 digitalWrite(0, LOW); 
 delay(300); //Repite 8
 
-digitalWrite(8, HIGH);
-digitalWrite(6, HIGH); 
+digitalWrite(7, HIGH);
+digitalWrite(5, HIGH); 
 digitalWrite(3, HIGH); 
 digitalWrite(1, HIGH); 
 delay(200);
+
 
 }
